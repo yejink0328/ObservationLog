aa7202cf6efc4f30d49052788b287f121262fffc
yejink0328 <129183269+yejink0328@users.noreply.github.com>
2023-08-21 20:25:07 +0900

    first commit

---
+++ ObservationLog/ObservationLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationLogApp.swift
+//  ObservationLog
+//
+//  Created by MAX on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ObservationLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
